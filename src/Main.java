64804ab547b79c733937c2f90b96b76c65197617
unknown <justyna.kuznikk@gmail.com>
2020-10-05 17:36:55 +0200

    Opening file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
         //Opening file
         File file = new File("fragment.txt");
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+        }
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        while (line = bufferedReader.readLine() != null){
+            
+
+        }
 
     }
 }
